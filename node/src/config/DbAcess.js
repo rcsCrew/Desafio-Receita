c84ad59b60e25cbbcd883cc121d5446ae9705349
ramon senger <51542841+rcsCrew@users.noreply.github.com>
2024-07-15 18:27:12 -0300

    Update DbAcess.js

--- node/src/config/DbAcess.js
+++ node/src/config/DbAcess.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "1234",
-  DB: "desafio",
+  USER: "",
+  PASSWORD: "",
+  DB: "",
   dialect: "mysql",
   pool: {
     max: 5,
